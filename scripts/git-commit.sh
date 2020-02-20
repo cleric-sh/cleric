15302177d8e210c7bf5722452bcd46fcad8ccd6a
Sebastian Nemeth <sebastian@monkmage.com>
2020-02-20 12:58:54 +1100

    update commit, push and workspace scripts

---
+++ scripts/git-commit.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source ${BASE_DIR}/.vars.sh
+
+set -e # Sets mode to exit on an error, without executing the remaining commands.
+
+# If we are in the master branch, or master branch on a remote,
+# exit to prevent accidental commits to the branch, which can
+# cause a few painful situations if the commit goes unnoticed.
+if [[ $BRANCH =~ (master$) ]]; then
+    echo "Detected accidental commit to master, exiting..."
+    exit 0
+fi
+
+# Extracts JIRA issue number from current branch name, if one exists.
+if [[ $BRANCH =~ ($JIRAKEY-[0-9]+) ]]; then 
+    export ISSUE=${BASH_REMATCH[1] };
+    echo "Detected JIRA issue: $ISSUE"
+fi
+
+if [ -z "$*" ]; then
+    "No commit message provided."
+    exit 0
+fi
+
+git add .
+git commit -m "$ISSUE$*"
--- scripts/workspace.sh
+++ scripts/workspace.sh
@@ -2,4 +2,15 @@
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${BASE_DIR}/.vars.sh
 
-yarn workspace @$STACK_NAME/$*
+# Extract the first argument as the workspace name param.
+name=$1
+shift 1
+
+# Match each workspace against the workspace name passed in as first param.
+yarn workspaces info | jq -r 'keys | .[]' | while IFS=$'\s' read -r workspace; do
+if [[ $workspace =~ $name ]]; then
+    # If the param matches a workspace, run the rest of the commands in the workspace.
+    echo "Running in workspace $workspace:"
+    yarn workspace $workspace "$@"
+fi
+done
