749ef8ff5871aaa82d982824eed7baa235cfb5d2
Sebastian Nemeth <sebastian@monkmage.com>
2020-02-21 15:15:41 +1100

    stub out a structure for filesystem structure declaration

--- packages/meta/src/index.ts
+++ packages/meta/src/index.ts
@@ -9,11 +9,13 @@ const packageJsonContent = packageJson`{
     "name": "testing"
 }`;
 
-const tsconfigContent = tsconfigJson({
-    compilerOptions: {
-        noImplicitAny: false
+const tsconfigContent = tsconfigJson`
+{
+    "compilerOptions": {
+        "noImplicitAny": false
     }
-})
+}
+`;
 
 const outPath = "~/Projects/experiments/output".replace("~", os.homedir());
 
@@ -21,19 +23,56 @@ const exists = promisify(fs.exists);
 const writeFile = promisify(fs.writeFile);
 const mkdir = promisify(fs.mkdir);
 
+type Context = {}
+
+type File = {
+    __type: "file";
+    (ctx: Context): {}
+}
+
+type FileBuilder = {
+    (name: string): File
+}
+
+const fil: FileBuilder = (name: string) => {
+    return {} as File;
+}
+
+
+type Directory = {
+    __type: "directory";
+    (ctx: Context): {}
+}
+
+type DirectoryBuilder = {
+    (name: string, nodes?: Array<Directory | File>): Directory
+}
+
+const dir: DirectoryBuilder = (name, nodes): Directory => {
+    return {} as Directory;
+}
+
+dir("", [
+    fil("package.json"),
+    fil("tsconfig.json"),
+    dir("src")
+])
+
+const root = (path: string, ) => {
+    // Resolve the '~' character to OS's home dir, if it is used.
+    path = path.replace("~", os.homedir());
+
+
+}
+
 const generate = async (filename: string, content: string) => {
     const filePath = path.join(outPath, filename);
 
-
-    const dirExists = await exists(outPath);
-
-    if (!dirExists) {
-        console.log("making...")
+    if (!await exists(outPath)) {
         await mkdir(outPath);
-        console.log("made...")
     }
 
-    writeFile(filePath, content);
+    await writeFile(filePath, content);
 }
 generate("package.json", packageJsonContent);
 generate("tsconfig.json", tsconfigContent);
