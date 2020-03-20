5e91274112aac04e2f141e44ed9310abef420714
Sebastian Nemeth <sebastian@monkmage.com>
2020-03-20 09:37:51 +0200

    refactor createSlice.spec

--- packages/store-experimental/src/slice/createSlice.spec.ts
+++ packages/store-experimental/src/slice/createSlice.spec.ts
@@ -4,29 +4,46 @@ import { BehaviorSubject } from 'rxjs';
 import { createSlice } from './createSlice';
 import { listen } from '@cleric/common';
 import { FooBar } from '../configs/test/types/FooBar';
+import { Slice } from './Slice';
 import '../configs/test';
 
 describe('createSlice', () => {
-  it('should create a Slice decorated with matching APIs', async () => {
 
-    const initial: t.TypeOf<typeof FooBar> = {
-      bar: 1,
-      foo: 'myString',
-    };
+  const initial: t.TypeOf<typeof FooBar> = {
+    bar: 1,
+    foo: 'myString',
+  };
 
-    const src = new BehaviorSubject(initial);
+  let node: Slice<'Test', typeof FooBar>;
+  let src: BehaviorSubject<t.TypeOf<typeof FooBar>>;
 
-    const node = createSlice(FooBar, src, 'Test');
+  beforeEach(() => {
 
+    src = new BehaviorSubject(initial);
+    node = createSlice(FooBar, src, 'Test');
+  })
+
+  it('should observe source values through $', async () => {
     const _values = listen(node.$);
-
     src.complete();
-
     expect(await _values).toEqual([initial]);
-    expect(node.doFoo()).toBe('Foo');
-    expect(node.doTest()).toBe('Test');
-    expect(node.doBar()).toBe('Bar');
-    expect(node.$configKey).toBe('Test');
-    expect(node.$type).toBe(FooBar);
   });
+
+  it('should load all configured APIs matching node type', () => {
+    expect(node.doFoo()).toBe('Foo');
+    expect(node.doBar()).toBe('Bar');
+  })
+
+  it('should have used configured slice node constructor', () => {
+    expect(node.doTest()).toBe('Test');
+  })
+
+  it('should return specified configuration key', () => {
+    expect(node.$configKey).toBe('Test');
+  })
+
+  it('should return specified node io-ts type', () => {
+    expect(node.$type).toBe(FooBar);
+  })
+
 });
