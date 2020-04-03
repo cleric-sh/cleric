92109e63a066a7a0d2faf392c108e7754ffbbe01
Sebastian Nemeth <sebastian@monkmage.com>
2020-04-03 15:57:33 +0300

    add sortier to lint:fix and lint-staged

--- packages/store-experimental/src/config/ConfigTypes.ts
+++ packages/store-experimental/src/config/ConfigTypes.ts
@@ -1,4 +1,5 @@
 /**
  * The register of all Config types available, keyed by ConfigKey.
  */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface ConfigTypes {}
--- packages/store-experimental/src/node/api/ApiTypes.ts
+++ packages/store-experimental/src/node/api/ApiTypes.ts
@@ -6,4 +6,5 @@ import {ConfigKey} from '../../config';
  * A register, by ApiKey, of the types that are used to augment SliceNode for
  * each Api.
  */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface ApiTypes<TConfigKey extends ConfigKey, TType extends t.Any> {}
