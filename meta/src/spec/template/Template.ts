08163ddb27ba4a73fc2a1aed90b59f1474de161e
Sebastian Nemeth <sebastian@monkmage.com>
2020-05-10 18:26:17 +0300

    use errors instead of __exports

--- meta/src/spec/template/Template.ts
+++ meta/src/spec/template/Template.ts
@@ -1,7 +1,7 @@
 import {Context} from './Context';
 
 export interface Template<TExports = {}> {
-  __exports: TExports; // Dummy field to disable intellisense wanting to remove TExports.
+  __errors: TExports;
   __type: 'Template';
   generate: (ctx: Context) => Promise<string>;
 }
