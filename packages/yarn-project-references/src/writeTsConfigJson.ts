faf3ed37f8155823fc4fa721652c249a918c40de
Sebastian Nemeth <sebastian@monkmage.com>
2020-05-15 20:26:10 +0300

    format as valid JSON

--- packages/yarn-project-references/src/writeTsConfigJson.ts
+++ packages/yarn-project-references/src/writeTsConfigJson.ts
@@ -1,5 +1,4 @@
 import {writeFileSync} from 'fs';
-import {stringify} from 'json5';
 import {join} from 'path';
 import {TSCONFIG_FILE_NAME, TsConfigJson} from './getTsConfigJson';
 
@@ -8,6 +7,6 @@ export const writeTsConfigJson = (
   tsConfigJson: TsConfigJson
 ) => {
   const path = join(wsRoot, TSCONFIG_FILE_NAME);
-  const content = stringify(tsConfigJson, null, 2);
+  const content = JSON.stringify(tsConfigJson, null, 2);
   writeFileSync(path, content);
 };
